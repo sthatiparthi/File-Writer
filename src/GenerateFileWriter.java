4544b22502d84d3c6340ddeeee4437204123bde0
sudhakar <sthatiparthi@castlighthealth.com>
2020-02-20 23:51:44 +0530

    Update GenerateFileWriter.java

--- src/GenerateFileWriter.java
+++ src/GenerateFileWriter.java
@@ -1,19 +1,28 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GenerateFileWriter {
 
   public static void main(String[] args) throws Exception {
     if (args.length > 0) {
-      int x = Integer.valueOf(args[0]);
+      final int BATCH_SIZE = 10000;
+      final int x = Integer.valueOf(args[0]);
       File file = new File("./output.txt");
       FileWriter fileWriter = new FileWriter(file, true);
       PrintWriter writer = new PrintWriter(fileWriter);
+      List<String> list = new ArrayList<>();
       for (int i = 0; i < x; i++) {
-        writer.println(generateUniqueString());
+        list.add(generateUniqueString());
+        if (BATCH_SIZE == list.size()) {
+          writer.print(list);
+          list.clear();
+        }
       }
+      writer.print(list);
       writer.close();
     }
   }
